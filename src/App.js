3fd880b1487e2ca6b4a33d82a61b8d7d26c960b2
Moaldahoul <ma811forever@gmail.com>
2018-06-11 11:11:39 +0200

    weather finder reactjs app

--- src/App.js
+++ src/App.js
@@ -16,56 +16,56 @@ class App extends React.Component{
     }
     
 
-    getWeather = async(e) => {
+    getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
 
         const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        if(city && country){
+        if (city && country) {
             this.setState({
                 temperature: data.main.temp,
                 city: data.name,
                 country: data.sys.country,
                 humidity: data.main.humidity,
                 description: data.weather[0].description,
-                error:""
+                error: ""
             });
-        }else{
+        } else {
             this.setState({
                 temperature: undefined,
                 city: undefined,
                 country: undefined,
                 humidity: undefined,
                 description: undefined,
-                error:" >> Please insert the City and the Country"
+                error: " >> Please insert the City and the Country"
             });
-            
+
         }
-        
+
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <div className="wrapper">
                     <div className="main">
                         <div className="container">
                             <div className="row">
                                 <div className="col-xs-S title-container">
-                                    <Titles/>
+                                    <Titles />
                                 </div>
                                 <div className="col-xs-7 form-container">
-                                <Form getWeather ={this.getWeather} />
-                                <Weather 
-                                    temperature={this.state.temperature} 
-                                    city={this.state.city} 
-                                    country={this.state.country}
-                                    humidity={this.state.humidity}
-                                    description={this.state.description}
-                                    error={this.state.error}
-                                    />    
-                                </div>   
+                                    <Form getWeather={this.getWeather} />
+                                    <Weather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description={this.state.description}
+                                        error={this.state.error}
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>
