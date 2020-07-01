67c9be2d809e1092ba4d2e3450e7e7be33b54d15
Mohamed <ma811forever@gmail.com>
2020-07-01 09:07:06 +0200

    remove JS code.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import "./App.css";
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
